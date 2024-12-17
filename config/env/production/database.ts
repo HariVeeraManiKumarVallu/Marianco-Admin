431c3b8bde0fc71e6c46a70b8f77217a4a1589cf
FaizanAsifB <faizanasif.dev@gmail.com>
2024-12-17 19:44:15 +0100

    add ssl false

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -7,7 +7,9 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
     },
     debug: false,
   },
