ad2c20639f9abb2a33868b117e25eed82ef7a3be
FaizanAsifB <faizanasif.dev@gmail.com>
2025-01-17 17:29:08 +0100

    update ssl env for database

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -7,9 +7,7 @@ export default ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
-        ca: env('DATABASE_CA', ''),
-      },
+      ssl: env.bool('DATABASE_SSL', false)
     },
     debug: false,
   },
